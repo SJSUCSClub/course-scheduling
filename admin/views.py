b9800880d3c6edd8ef71ca33f3cec6fb35cc2652
chrehall68 <chrehall68@gmail.com>
2025-02-04 10:11:52 -0800

    decorate admin endpoints with try_response

--- admin/views.py
+++ admin/views.py
@@ -5,7 +5,7 @@ from authentication.permissions import (
     ModeratorPermission,
 )
 from django.http import JsonResponse
-from core.views.utils import validate_user, validate_body, validate_page_limit
+from core.views.utils import validate_user, validate_body, validate_page_limit, try_response
 from admin.services.admins import (
     is_admin,
     admin_manage_moderator,
@@ -17,6 +17,7 @@ from admin.services.admins import (
 
 @api_view(["GET"])
 @permission_classes([AuthenticatedPermission])
+@try_response
 def verify_view(request):
     user_id = validate_user(request)
     response_data = {"isAdmin": is_admin(user_id)}
@@ -25,6 +26,7 @@ def verify_view(request):
 
 @api_view(["GET"])
 @permission_classes([AuthenticatedPermission, ModeratorPermission])
+@try_response
 def flagged_reviews_view(request):
     json_data = get_paginated_flagged_reviews(
         flag_status=request.GET.get("status") or "Pending",
@@ -36,6 +38,7 @@ def flagged_reviews_view(request):
 
 @api_view(["POST"])
 @permission_classes([AuthenticatedPermission, ModeratorPermission])
+@try_response
 def manage_review_view(request, review_id):
     results = manage_flagged_review(review_id=review_id, action=request.data["action"])
     return JsonResponse(results)
@@ -43,6 +46,7 @@ def manage_review_view(request, review_id):
 
 @api_view(["POST"])
 @permission_classes([AuthenticatedPermission, AdminPermission])
+@try_response
 def manage_moderator_view(request):
     data = validate_body(request)
     results = admin_manage_moderator(data)
@@ -51,6 +55,7 @@ def manage_moderator_view(request):
 
 @api_view(["GET"])
 @permission_classes([AuthenticatedPermission, AdminPermission])
+@try_response
 def list_view(request):
     # validate view-specific parameters
     sort_by = request.GET.get("sort_by", None)
