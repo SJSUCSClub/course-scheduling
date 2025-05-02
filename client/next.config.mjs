f58de1fbdd9522b4876fdd04feccd8bf5d0cb6e5
Justin <justlee888@gmail.com>
2025-05-01 20:41:23 -0700

    add var in .env called ENABLE_HOT_RELOAD_POLLING

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
+const enablePolling = process.env.ENABLE_HOT_RELOAD_POLLING === 'true';
 
 const nextConfig = {
     reactStrictMode: true,
     webpack(config, { dev, isServer }) {
-      if (dev && isServer) {
+      if (dev && isServer && enablePolling) {
         config.watchOptions = {
           poll: 1000,
           aggregateTimeout: 300,
