a303fff4028356533b44fd0ca56befdc79844b0e
Justin Lee <38022905+Jjmaxxx@users.noreply.github.com>
2025-09-05 18:02:25 -0700

    Feature/hot reload windows (#24)
    * force recompile by watching changes every second in nextConfig (windows hot reloading issue)
    
    * revert
    
    * add var in .env called ENABLE_HOT_RELOAD_POLLING
    
    * update .env.example

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const enablePolling = process.env.ENABLE_HOT_RELOAD_POLLING === 'true';
+
+const nextConfig = {
+    reactStrictMode: true,
+    webpack(config, { dev, isServer }) {
+      if (dev && isServer && enablePolling) {
+        config.watchOptions = {
+          poll: 1000,
+          aggregateTimeout: 300,
+        };
+      }
+      return config;
+    },
+  };
 
 export default nextConfig;
